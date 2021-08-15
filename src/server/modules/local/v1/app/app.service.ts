7acdf350f3d29acfb80c2994e6eb526637746903
Cherepanov Pavel <P.Cherepanov@hostco.ru>
2021-08-16 04:14:22 +0500

    убрал оставшиеся во время отладки console.log'и

--- src/server/modules/local/v1/app/app.service.ts
+++ src/server/modules/local/v1/app/app.service.ts
@@ -7,13 +7,11 @@ export class AppService {
   private readonly npm_package_version: string;
 
   constructor(private readonly configService: ConfigService) {
-    console.log(`PORT ${configService.get('PORT')}`);
     this.npm_package_name = configService.get<string>('npm_package_name');
     this.npm_package_version = configService.get<string>('npm_package_version');
   }
 
   getApp(): Record<string, any> {
-    console.log(process.env);
     return {
       name: this.npm_package_name,
       version: this.npm_package_version,
