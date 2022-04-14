445f9068596ce0135126688496209964aa5eadb4
Popov Andrey <a.popov@hostco.ru>
2022-04-14 15:55:45 +0300

    #87372 Рефакторинг шаблона

--- src/common/utils/time/index.ts
+++
@@ -1,9 +0,0 @@
-import * as moment from 'moment/moment';
-import { Moment } from 'moment';
-
-export function momentWithOffset(
-  date: string | Moment = moment(),
-  utcOffset?: string,
-): Moment {
-  return moment(date).utcOffset(utcOffset || null);
-}
